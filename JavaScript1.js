a7d6959f5be49af139fb2c012e66af06fb75c548
langroves <118496013+langroves@users.noreply.github.com>
2024-02-20 23:05:54 -0600

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,13 @@
+﻿// JavaScript code to toggle between background images
+const backgroundImages = document.querySelectorAll('.background-image');
+let currentIndex = 0;
+
+function transitionBackground() {
+    const nextIndex = (currentIndex + 1) % backgroundImages.length;
+    backgroundImages[currentIndex].style.transform = 'translateX(-100%)';
+    backgroundImages[nextIndex].style.transform = 'translateX(0%)';
+    currentIndex = nextIndex;
+}
+
+// Transition the background every 5 seconds
+setInterval(transitionBackground, 5000);
